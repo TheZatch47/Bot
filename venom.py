d2c531ee57f60089c78ed3cc29b31b61956538a3
Adnan Ansari <87154861+TheZatch47@users.noreply.github.com>
2024-08-05 04:29:58 +0530

    venom.py

--- venom.py
+++ venom.py
@@ -4,8 +4,8 @@ import logging
 from aiogram import Bot
 import asyncio
 
-API_TOKEN = 'TOKEN'
-ADMIN_ID = 'YOUR ID'
+API_TOKEN = '7026609274:AAEkIO_w1YxmKVY80X_lqN1wnvy9vTHP8hQ'
+ADMIN_ID = '5848883943'
 MAX_RESTARTS = 5
 RESTART_PERIOD = 60  # Seconds
 
